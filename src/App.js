7d34866cba89fc42669134b6c256b903adffda86
NatDing <dingkenseannat@gmail.com>
2022-02-21 23:03:54 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,237 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  }
+];
+
+const soundsName = {
+  heaterKit: "Heater Kit",
+  smoothPianoKit: "Smooth Piano Kit"
 }
 
-export default App;
+const soundsGroup = {
+  heaterKit: bankOne,
+  smoothPianoKit: bankTwo
+}
+
+const KeyboardKey = ({ play, sound: {keyTrigger,url,keyCode,id} }) => { 
+  
+  const handleKeydown = (event) => {
+   if(event.keyCode === keyCode){
+     play(keyTrigger, id)
+   }
+  }
+
+  React.useEffect(() => {
+   document.addEventListener("keydown", handleKeydown)
+  })
+  return(
+  <button value="test" id={keyCode} className="drum-pad" onClick={() => play(keyTrigger,id)}>
+  <audio className="clip" id={keyTrigger} src={url} />
+  {keyTrigger}
+  </button>
+  )
+}
+
+const Keyboard = ({ play, sounds, power }) => {
+  if(power === true){
+    return(<div className="keyboard">
+      {sounds.map((sound) => <KeyboardKey play={play} sound={sound} />)}
+      </div>)
+} else { return (<div className="keyboard">
+      {sounds.map((sound) => <KeyboardKey play={play} sound={{...sound, url: "#"}} />)}
+      </div>)
+
+ }
+}
+const Control = ({ power, powerChange, volume, handleVolumeChange, name, changeSounds }) => {
+  return <div className="control">
+   <button onClick={powerChange}>Power: {power ? "ON" : "OFF"}</button>
+   <h2>Volume: {Math.round(volume * 100)}</h2>
+   <input max="1" min="0" step="0.01" type="range" value={volume} onChange={handleVolumeChange} />
+   <h2 id="display">{name}</h2>
+   <button onClick={changeSounds}>Change Kit</button>   
+  </div>
+}
+
+
+const App = () => {
+  const [power, setPower] = React.useState(true);
+  const [volume, setVolume] = React.useState(1);
+  const [soundName, setSoundName] = React.useState("") ;
+  const [soundType, setSoundType] = React.useState("heaterKit");
+  const [sounds, setSounds] = React.useState(soundsGroup[soundType]);
+
+  const powerChange = () => {
+    setPower(!power)
+  }
+
+  const handleVolumeChange = (event) => {
+    setVolume(event.target.value)
+  }
+
+  const styleActiveKey = (audio) => {
+    audio.parentElement.style.backgroundColor = "#000000"
+    audio.parentElement.style.color = "#ffffff"
+  }
+
+  const styleDeactiveKey = (audio) => {
+    setTimeout(() => {
+      audio.parentElement.style.backgroundColor = "#ffffff"
+      audio.parentElement.style.color = "#000000"
+    }, 100)
+  }
+  
+  const setKeyVolume = () => {
+    const audios = sounds.map(sound => document.getElementById(sound.keyTrigger))
+     audios.forEach(audio => {
+       if(audio){
+         audio.volume = volume
+       }
+     })
+  }
+
+  const play = (keyTrigger, id) => {
+   setSoundName(id)
+   const audio = document.getElementById(keyTrigger)
+   styleActiveKey(audio)
+   audio.currentTime = 0
+   audio.play()
+   styleDeactiveKey(audio)
+  }
+  
+  const changeSounds = () => {
+    setSoundName("")
+    if(soundType === "heaterKit"){
+      setSoundType("smoothPianoKit")
+      setSounds(soundsGroup.smoothPianoKit)      
+    } else {
+      setSoundType("heaterKit")
+      setSounds(soundsGroup.heaterKit)
+    }
+
+  }
+
+    return( 
+    <div id="drum-machine">
+      {setKeyVolume()}
+      <div className="wrapper">
+        <Keyboard power={power} play={play} sounds={sounds}/>
+        <Control powerChange={powerChange} power={power} volume={volume} handleVolumeChange={handleVolumeChange} name={soundName || soundsName[soundType]} changeSounds={changeSounds}/>
+      </div>
+    </div>
+    )};
+
+  
+export default App
